94ecb5943ce85fe8260aa6d245eb199a55843203
Hendi Santika <hendisantika@gmail.com>
2018-09-23 06:57:56 +0700

    #8 - Add Integration Test

---
+++ src/test/java/com/hendisantika/springbootpostgressample/controller/HomeControllerTest.java
@@ -0,0 +1,43 @@
+package com.hendisantika.springbootpostgressample.controller;
+
+import com.hendisantika.springbootpostgressample.domain.AppUser;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgres-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/09/18
+ * Time: 06.56
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = {"classpath:application-test.yml"})
+public class HomeControllerTest {
+    RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void shouldAdd_AppUser_ToDb() {
+
+        ResponseEntity<AppUser> responseEntity = restTemplate
+                .postForEntity("http://localhost:9000/user/Abderrazak BOUADMA",
+                        MockHttpServletRequest.DEFAULT_PROTOCOL,
+                        AppUser.class);
+
+        final AppUser appUser = responseEntity.getBody();
+
+        Assertions.assertThat(appUser).isNotNull();
+        Assertions.assertThat(appUser.getUsername()).isNotNull().isEqualTo("Abderrazak BOUADMA");
+    }
+}
