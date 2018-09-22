507d48b89ba3b6b99683433d299455427fa64ed7
Hendi Santika <hendisantika@gmail.com>
2018-09-23 06:32:31 +0700

    #1 - First commit

---
+++ src/main/java/com/hendisantika/springbootpostgressample/SpringBootPostgresSampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootpostgressample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgresSampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/springbootpostgressample/SpringBootPostgresSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootpostgressample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPostgresSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
