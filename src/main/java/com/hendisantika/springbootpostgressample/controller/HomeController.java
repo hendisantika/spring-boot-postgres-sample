c7b7bfb68fe81534fdbca9bb1e902e652604a61a
Hendi Santika <hendisantika@gmail.com>
2018-09-23 06:38:44 +0700

    #5 - Add Home Controller

---
+++ src/main/java/com/hendisantika/springbootpostgressample/controller/HomeController.java
@@ -0,0 +1,62 @@
+package com.hendisantika.springbootpostgressample.controller;
+
+import com.hendisantika.springbootpostgressample.domain.AppUser;
+import com.hendisantika.springbootpostgressample.repository.UserRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgres-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/09/18
+ * Time: 06.37
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HomeController {
+
+    final UserRepository userRepository;
+
+    @Inject
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping(value = "/hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok().body("Hello there !");
+    }
+
+    @PostMapping(value = "/user/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AppUser> create(@PathVariable String username) {
+        AppUser appUser = new AppUser();
+        appUser.setUsername(username);
+        AppUser saved = userRepository.save(appUser);
+        return ResponseEntity.ok().body(saved);
+    }
+
+    @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<AppUser>> findAll() {
+        final List<AppUser> resultList = new ArrayList<>();
+        final Iterable<AppUser> all = userRepository.findAll();
+        all.forEach(resultList::add);
+        return ResponseEntity.ok().body(resultList);
+    }
+
+}
+
